237d896c29a3a7969c154c4cb4def3c1d291fae9
abhishek rana <mrabhishekrana123@gmail.com>
2024-06-03 11:40:58 +0530

    display logined username on dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,11 +11,20 @@ export class DashboardComponent {
 
   isRegistered = signal(false);
   isLoggedIn = signal(false);
-
+  name: string = '';
+  userDetail: any;
+  user: { userName: string, password: string, role: string, profileName: string } = {
+    userName: '',
+    password: '',
+    role: '',
+    profileName: ''
+  };
 
   constructor(private sharedService: SharedService, private router: Router) { }
 
   ngOnInit() {
+    this.userDetail = sessionStorage.getItem('userInfo');
+    this.user = JSON.parse(this.userDetail);
     this.isRegistered = this.sharedService.isRegistered;
     this.isLoggedIn = this.sharedService.isLoggedIn;
   }
