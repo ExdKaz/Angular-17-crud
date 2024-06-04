fb6ff19cdb471b66856ec452956355d5cb278c03
abhishek rana <mrabhishekrana123@gmail.com>
2024-06-04 15:49:43 +0530

    reusable table created with sorting pagination and filter

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MaterialModule } from 'src/app/material/material.module';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, CommonModule, TitleCasePipe],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
-export class TableComponent {
+export class TableComponent implements AfterViewInit {
+  @Input() columns: string[] = [];
+  @Input() dataSource = new MatTableDataSource<any>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
